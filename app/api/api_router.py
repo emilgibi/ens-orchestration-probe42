02afa0d180da1026118559f6bb6017b4ceb65abd
Emil Gibi <emil.gibi@gmail.com>
2026-07-17 11:43:03 +0000

    location risk endpoint

--- app/api/api_router.py
+++ app/api/api_router.py
@@ -4,6 +4,6 @@ from app.api.endpoints import analysis, procurevision, sse, locationrisk
 
 api_router = APIRouter()
 api_router.include_router(analysis.router, prefix="/analysis", tags=["analysis"])
-api_router.include_router(locationrisk.router, tags=["locationrisk"])
+api_router.include_router(locationrisk.router, prefix="/location-risk", tags=["locationrisk"])
 api_router.include_router(procurevision.router, tags=["procurevision"])
 api_router.include_router(sse.router, prefix="/status", tags=["status"])
